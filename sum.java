7bde1787b3afea44c696f7e2a2c8add6c7a9dcf6
root <root@ip-172-31-2-232.ap-south-1.compute.internal>
2026-07-13 07:14:45 +0000

    Executing code submission: sum.java

---
+++ sum.java
@@ -0,0 +1,8 @@
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+    }
+}
