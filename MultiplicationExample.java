1e8802a7332058af9daecf4bfdb38e103c0eb4c3
root <root@ip-172-31-2-232.ap-south-1.compute.internal>
2026-07-14 13:28:24 +0000

    Execute MultiplicationExample.java

---
+++ MultiplicationExample.java
@@ -0,0 +1,13 @@
+public class MultiplicationExample {
+    public static void main(String[] args) {
+        // Hardcoded variables (no user input required)
+        int firstNumber = 15;
+        int secondNumber = 4;
+        
+        // Performing multiplication using the * operator
+        int product = firstNumber * secondNumber;
+        
+        // Printing the final result
+        System.out.println("The product of " + firstNumber + " and " + secondNumber + " is: " + product);
+    }
+}
