8efdd781c217723e13d68521e9aa61044ab07c40
root <root@ip-172-31-2-232.ap-south-1.compute.internal>
2026-07-15 04:35:31 +0000

    Execute sum.java

---
+++ java/sum.java
@@ -0,0 +1,8 @@
+// Make sure your class name matches the filename exactly!
+public class sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+    }
+}
--- python/kaustubh.py
+++
@@ -1,8 +0,0 @@
-# Dynamic AWS Execution Engine Test
-def main():
-    a = 5
-    b = 10
-    print(f"The sum of {a} and {b} is {a + b}")
-
-if __name__ == "__main__":
-    main()
