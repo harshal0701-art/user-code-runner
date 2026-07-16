d51452318530dc60c938ac4e294cc5420d3e1572
root <root@ip-172-31-2-232.ap-south-1.compute.internal>
2026-07-16 13:13:18 +0000

    Execute him.java

---
+++ java/him.java
@@ -0,0 +1,8 @@
+// Make sure your class name matches the filename exactly!
+public class him {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+    }
+}
